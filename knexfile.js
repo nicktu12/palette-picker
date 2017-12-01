f8cbd540aa1563a46a22232e3a83d112a89616fc
Nick Teets <nickteets@gmail.com>
2017-12-01 10:29:56 -0700

    Unbreak stuff in knex config

--- knexfile.js
+++ knexfile.js
@@ -15,7 +15,7 @@ module.exports = {
   },
 
   production: {
-    client: 'p',
+    client: 'pg',
     connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
       directory: '.db/migrations'
