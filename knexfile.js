a558746e1a8161ae96c60561864d17aa014c6443
Nick Teets <nickteets@gmail.com>
2017-12-04 09:28:28 -0700

    Update circle.yml for database

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,7 @@ module.exports = {
 
   test: {
     client: 'pg',
-    connection: 'postgres://localhost/palettepickertest',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/palettepickertest',
     migrations: {
       directory: './db/migrations'
     },
