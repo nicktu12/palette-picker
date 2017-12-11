3ddf849d90d030a62e9aef418e33407cbf9fd780
Nick Teets <nickteets@gmail.com>
2017-12-10 20:18:07 -0700

    Tweak indexedDB things in script.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,7 @@
 
 this.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('assets-v2').then(cache => {
+    caches.open('assets-v3').then(cache => {
       return cache.addAll([
         '/',
         '/js/scripts.js',
@@ -22,7 +22,7 @@ this.addEventListener('fetch', event => {
 });
 
 this.addEventListener('activate', event => {
-  let cacheWhitelist = ['assets-v2'];
+  let cacheWhitelist = ['assets-v3'];
 
   event.waitUntil(
     caches.keys().then(keyList => {
