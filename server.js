0ff8c75636175085ff5c1d2d01f3b41fef982fc8
Nick Teets <nickteets@gmail.com>
2018-03-13 12:50:26 -0600

    Remove default server response for / endpoint

--- server.js
+++ server.js
@@ -27,10 +27,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("port", process.env.PORT || 3000);
 app.locals.title = "Palette Picker";
 
-app.get("/", (request, response) => {
-  response.send(`What's good palette people?`);
-});
-
 app.listen(app.get("port"), () => {
   console.log(`${app.locals.title} is running on ${app.get("port")}.`);
 });
